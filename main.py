dc19dae479864d130bf960d94814430a65e43846
Morgan <ketzatl@hotmail.fr>
2020-02-27 13:58:46 +0100

    -Déplacement des class Player et Game dans leurs propres fichiers --> game.py et player.py

--- main.py
+++ main.py
@@ -1,29 +1,8 @@
 import pygame
+from game import Game
 pygame.init()
 
 
-# ---------- Création de la classe "Game" ------------------
-
-class Game:
-
-    def __init__(self):
-# -------- Générer notre joueur lors d'une nouvelle partie
-        self.player = Player()
-
-# ---------- Création de la classe "Player" ------------------
-
-class Player(pygame.sprite.Sprite):
-
-    def __init__(self):
-        super().__init__()
-        self.health = 100
-        self.max_health = 100
-        self.attack = 10
-        self.velocity = 5
-        self.image = pygame.image.load('assets/player.png')
-        self.rect = self.image.get_rect()
-        self.rect.x = 400
-        self.rect.y = 500
 
 
 # ---------- Création de la fenêtre de jeu ------------------
